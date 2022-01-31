cd80eeec4fd0cd06562eedd8a3debf2c61cf2d53
Sheldon Bean <sheldon.bean@gmail.com>
2022-01-30 19:23:31 -0700

    Beginner Scripting Translate and Rotate

---
+++ Beginner Scripting Scripts/Assets/Scripts/TransformFunctions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 10f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * (moveSpeed * Time.deltaTime));
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed*Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed*Time.deltaTime);
+        
+
+    }
+}
