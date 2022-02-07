2b47d6ea00017a6372d784710c6343eb4a22a92e
Shelldin <sheldon.bean@gmail.com>
2022-02-07 16:51:20 -0700

    MV Tutorial: Added BulletController and Bullet Animations

---
+++ Metroidvania Tutorial/Assets/Scripts/BulletController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed;
+    public Rigidbody2D bulletRB;
+
+    public Vector2 moveDir;
+}
