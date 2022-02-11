fac69df3032885a24457feddbb2d77ccc6a888b6
Shelldin <sheldon.bean@gmail.com>
2022-02-11 04:58:06 -0700

    MV Tutorial: added dash ability and dash after image to PlayerController

--- Metroidvania Tutorial/Assets/Scripts/PlayerController.cs
+++ Metroidvania Tutorial/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 //from Metroidvania Tutorial on Udemy https://www.udemy.com/course/unity-metvania/
 public class PlayerController : MonoBehaviour
@@ -22,19 +23,63 @@ public class PlayerController : MonoBehaviour
         
         private bool canDoubleJump;
 
+        public float dashSpeed,
+                dashTime;
+        private float dashCounter;
+
+        public SpriteRenderer playerSR,
+                afterImage;
+
+        public float afterImageLifeTime,
+                timeBetweenAfterImages;
+        private float afterImageCounter;
+        public Color afterImageColor;
+
+
         private void Update()
         {
-                //horizontal movement
-                playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*moveSpeed, playerRB.velocity.y);
-                
-                //controlling direction of the character
-                if (playerRB.velocity.x < 0)
+                //checks if player is dashing
+                if (Input.GetButtonDown("Fire2"))
                 {
-                        transform.localScale = new Vector3(-1f, 1f, 1f);
+                        dashCounter = dashTime;
+                        
+                        ShowAfterImage();
                 }
-                else if(playerRB.velocity.x > 0)
+                
+                
+                //dash movement
+                if (dashCounter > 0)
                 {
-                        transform.localScale = Vector3.one;
+                        //resolving duration of dash
+                        dashCounter -= Time.deltaTime;
+
+                        playerRB.velocity = new Vector2(dashSpeed * transform.localScale.x, 0);
+
+                        afterImageCounter -= Time.deltaTime;
+                        if (afterImageCounter <= 0)
+                        {
+                                ShowAfterImage();
+                        }
+                }
+                //if player is not dashing they can move
+                //(player does not have horizontal movement control during a dash)
+                else
+                {
+
+                        //horizontal movement
+                        playerRB.velocity =
+                                new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRB.velocity.y);
+
+                        //controlling direction of the character
+                        if (playerRB.velocity.x < 0)
+                        {
+                                transform.localScale = new Vector3(-1f, 1f, 1f);
+                        }
+                        else if (playerRB.velocity.x > 0)
+                        {
+                                transform.localScale = Vector3.one;
+                        }
+
                 }
 
                 //checking if player is on ground or not
@@ -43,6 +88,7 @@ public class PlayerController : MonoBehaviour
                 //Jumping
                 if (Input.GetButtonDown("Jump") && (isOnGround || canDoubleJump))
                 {
+                        //Resolving double jump attempts
                         if (isOnGround)
                         { 
                                 canDoubleJump = true; 
@@ -50,6 +96,8 @@ public class PlayerController : MonoBehaviour
                         else
                         {
                                 canDoubleJump = false;
+                                
+                                anim.SetTrigger("doubleJump");
                         }
                                 
                         
@@ -69,5 +117,18 @@ public class PlayerController : MonoBehaviour
                 
                 anim.SetBool("isOnGround", isOnGround);
                 anim.SetFloat("speed", Mathf.Abs(playerRB.velocity.x));
+        }
+
+        //function to create after images when dashing
+        public void ShowAfterImage()
+        {
+           SpriteRenderer image =  Instantiate(afterImage, transform.position, transform.rotation);
+           image.sprite = playerSR.sprite;
+           image.transform.localScale = transform.localScale;
+           image.color = afterImageColor;
+           
+           Destroy(image.gameObject, afterImageLifeTime);
+
+           afterImageCounter = timeBetweenAfterImages;
         }
 }
