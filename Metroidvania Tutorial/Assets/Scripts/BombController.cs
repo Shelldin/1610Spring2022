522486e70ae3ef00c9eb10a6015989b0589c6b93
Shelldin <sheldon.bean@gmail.com>
2022-02-15 05:18:35 -0700

    MV Tutorial: added destructive functionality to bomb in BombController

--- Metroidvania Tutorial/Assets/Scripts/BombController.cs
+++ Metroidvania Tutorial/Assets/Scripts/BombController.cs
@@ -8,8 +8,12 @@ public class BombController : MonoBehaviour
     public float timeToExplode = .5f;
     public GameObject explosion;
 
+    public float blastRange;
+    public LayerMask whatIsDestructible;
+
     private void Update()
     {
+        //bomb explosion
         timeToExplode -= Time.deltaTime;
         if (timeToExplode <= 0)
         {
@@ -19,6 +23,18 @@ public class BombController : MonoBehaviour
             }
             
             Destroy(gameObject);
+            
+            //find all destructible colliders in blast range
+           Collider2D[] objectsToRemove = Physics2D.OverlapCircleAll(transform.position, blastRange, whatIsDestructible);
+
+           //destroy each gameObject found in objectsToRemove
+           if (objectsToRemove.Length > 0)
+           {
+               foreach (Collider2D col in objectsToRemove)
+               {
+                   Destroy(col.gameObject);
+               }
+           }
         }
     }
 }
