466a4c51e85590f3b7a3528162702bcb675f8f45
Shelldin <sheldon.bean@gmail.com>
2022-02-22 22:23:48 -0700

    MV Tutorial: added movement, ground check, and jumping EnemyPatroller

--- Metroidvania Tutorial/Assets/Scripts/EnemyPatroller.cs
+++ Metroidvania Tutorial/Assets/Scripts/EnemyPatroller.cs
@@ -14,17 +14,34 @@ public class EnemyPatroller : MonoBehaviour
         waitAtPointTime;
      private float waitTimeCounter;
 
+     //jumping variables
     public float jumpForce;
+    public Transform groundPoint;
+    public LayerMask groundLayers;
+    private bool isGrounded;
 
     public Rigidbody2D enemyRB;
+    
+    //animation variables
+    public Animator anim;
+    
 
     private void Start()
     {
         waitTimeCounter = waitAtPointTime;
+
+        foreach (Transform pPoint in patrolPoints)
+        {
+            pPoint.SetParent(null);
+        }
     }
 
     private void Update()
     {
+        //check if enemy is on ground
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, groundLayers);
+        
+        //moving to next patrol point
         //check distance between patrol point and enemy and move if distance is too great
         if (Mathf.Abs(transform.position.x - patrolPoints[currentPoint].position.x) > .2f)
         {
@@ -33,12 +50,41 @@ public class EnemyPatroller : MonoBehaviour
             {
                 //right movement
                 enemyRB.velocity = new Vector2(moveSpeed, enemyRB.velocity.y);
+                transform.localScale = new Vector3(-1, 1, 1);
             }
             else
             {
                 //left movement
                 enemyRB.velocity = new Vector2(-moveSpeed, enemyRB.velocity.y);
+                transform.localScale = Vector3.one;
+            }
+
+            if (transform.position.y < patrolPoints[currentPoint].position.y -.5f && isGrounded)
+            {
+                enemyRB.velocity = new Vector2(enemyRB.velocity.x, jumpForce);
             }
         }
+        //waiting when reaching a patrol point
+        else
+        {
+            enemyRB.velocity = new Vector2(0f, enemyRB.velocity.y);
+
+            waitTimeCounter -= Time.deltaTime;
+            
+            //reset time to wait and target next patrol point to move to
+            if (waitTimeCounter <= 0 )
+            {
+                waitTimeCounter = waitAtPointTime;
+
+                currentPoint++;
+                
+                //resetting the current patrol point if the length is reached
+                if (currentPoint >= patrolPoints.Length)
+                {
+                    currentPoint = 0;
+                }
+            }
+        }
+        anim.SetFloat("speed", Mathf.Abs(enemyRB.velocity.x));
     }
 }
