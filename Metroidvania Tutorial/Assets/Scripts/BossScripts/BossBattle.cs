a6d11daffffc4931c20f9825ac112e7ab3cfa3ad
Shelldin <sheldon.bean@gmail.com>
2022-04-22 05:34:36 -0600

    Phase 2 and 3 shooting added

--- Metroidvania Tutorial/Assets/Scripts/BossScripts/BossBattle.cs
+++ Metroidvania Tutorial/Assets/Scripts/BossScripts/BossBattle.cs
@@ -63,13 +63,18 @@ public class BossBattle : MonoBehaviour
         {
             if (activeCounter> 0)
             {
+                
+                //countdown before fading out
                 activeCounter -= Time.deltaTime;
+                
+                //reset fadeCounter
                 if (activeCounter <= 0)
                 {
                     fadeCounter = fadeOutTime;
                     anim.SetTrigger("vanish");
                 }
                 
+                //fireball shooting behavior
                 shotCounter -= Time.deltaTime;
                 if (shotCounter <= 0)
                 {
@@ -78,9 +83,14 @@ public class BossBattle : MonoBehaviour
                     Instantiate(bullet, shotPoint.position, Quaternion.identity);
                 }
             }
+            
+            //fading out after activeCounter reaches 0
             else if(fadeCounter > 0)
             {
+                //fade out countdown
                 fadeCounter -= Time.deltaTime;
+                
+                //set boss inactive while faded out and reset inactiveCounter
                 if (fadeCounter <= 0)
                 {
                     boss.gameObject.SetActive(false);
@@ -88,45 +98,83 @@ public class BossBattle : MonoBehaviour
                     inactiveCounter = inactiveTime;
                 }
             }
+            
+            //being inactive after fadeCounter reaches 0
             else if (inactiveCounter>0)
             {
+                
+                //inactive countdown
                 inactiveCounter -= Time.deltaTime;
+                
+                //re-activate boss at new spawn point when inactiveCounter reaches 0
                 if (inactiveCounter <= 0)
                 {
                     boss.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
                     boss.gameObject.SetActive(true);
 
+                    //reset activeCounter
                     activeCounter = acitveTime;
 
+                    //reset shotCounter
                     shotCounter = slowTimeBetweenShots;
                 }
             }
         }
-        //phase 2 behavior
+        //phase 2 & 3 behavior
         else
         {
+            
+            //set target point to boss's position if targetPoint is null
             if (targetPoint == null)
             {
                 targetPoint = boss;
                 fadeCounter = fadeOutTime;
                 anim.SetTrigger("vanish");
             }
+            
+            
             else
             {
+                
+                //if boss isn't at current target point, move towards target point
                 if (Vector3.Distance(boss.position, targetPoint.position) > .02f)
                 {
                     boss.position =
                         Vector3.MoveTowards(boss.position, targetPoint.position, moveSpeed * Time.deltaTime);
                     
+                    //when boss reaches target point reset fadeCounter and trigger vanish animation
                     if (Vector3.Distance(boss.position, targetPoint.position) <= .02f)
                     {
                         fadeCounter = fadeOutTime;
                         anim.SetTrigger("vanish");
                     }
+                    
+                    shotCounter -= Time.deltaTime;
+                    if (shotCounter <= 0)
+                    {
+                        //while in phase 2 health range use slower shots time
+                        if (BossHealthContoller.instance.currentHealth > phase3Int)
+                        {
+                            shotCounter = slowTimeBetweenShots;
+                        }
+                        //if below phase 2 health range use fast shot time (phase 3)
+                        else
+                        {
+                            shotCounter = fastTimeBetweenShots;
+                        }
+
+                        Instantiate(bullet, shotPoint.position, Quaternion.identity);
+                    }
                 }
+
+                //fade out behavior
                 else if(fadeCounter > 0)
                 {
+                    
+                    //fade out countdown
                     fadeCounter -= Time.deltaTime;
+                    
+                    //when fadeCounter reaches 0 reset set boss inactive and reset inactiveCounter
                     if (fadeCounter <= 0)
                     {
                         boss.gameObject.SetActive(false);
@@ -134,9 +182,14 @@ public class BossBattle : MonoBehaviour
                         inactiveCounter = inactiveTime;
                     }
                 }
+                
+                //inacitve behaviour
                 else if (inactiveCounter>0)
                 {
+                    //inactive countdown
                     inactiveCounter -= Time.deltaTime;
+                    
+                    //when inactiveCounter reaches 0 boss reappears at new point and selects new target position
                     if (inactiveCounter <= 0)
                     {
                         boss.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
@@ -144,6 +197,8 @@ public class BossBattle : MonoBehaviour
                         targetPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
 
                         int whileBreaker = 0;
+                        
+                        //choose new targetpoint if boss selects the target point he's already at
                         while (targetPoint.position == boss.position && whileBreaker < 100)
                         {
                             targetPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
@@ -151,7 +206,19 @@ public class BossBattle : MonoBehaviour
                             whileBreaker++;
                         }
                         
+                        //boss reappears
                         boss.gameObject.SetActive(true);
+                        
+                        //while in phase 2 health range use slower shots time
+                        if (BossHealthContoller.instance.currentHealth > phase3Int)
+                        {
+                            shotCounter = slowTimeBetweenShots;
+                        }
+                        //if below phase 2 health range use fast shot time (phase 3)
+                        else
+                        {
+                            shotCounter = fastTimeBetweenShots;
+                        }
                         
                     }
                 }
