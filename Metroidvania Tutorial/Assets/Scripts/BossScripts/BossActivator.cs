1fcac02189c8a6ce295a42b9a51b84983ab2eb83
Shelldin <sheldon.bean@gmail.com>
2022-04-22 01:46:43 -0600

    Created BossActivator and BossBattle Scripts

---
+++ Metroidvania Tutorial/Assets/Scripts/BossScripts/BossActivator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossActivator : MonoBehaviour
+{
+
+    public GameObject bossToActivate;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            bossToActivate.SetActive(true);
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Metroidvania Tutorial/Assets/Scripts/BossScripts/BossBattle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBattle : MonoBehaviour
+{
+    private CameraController cam;
+
+    public Transform camPosition;
+
+    public float camSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<CameraController>();
+
+        //disabling CameraController so it stops following player during boss battle
+        cam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.position =
+            Vector3.MoveTowards(cam.transform.position, camPosition.position, camSpeed * Time.deltaTime);
+    }
+}
