400bb7507ea5cc28b35c5c4887d7d8dc4573fcce
Shelldin <sheldon.bean@gmail.com>
2022-04-22 03:24:14 -0600

    Boss Phase 1 movement

--- Metroidvania Tutorial/Assets/Scripts/BossScripts/BossBattle.cs
+++ Metroidvania Tutorial/Assets/Scripts/BossScripts/BossBattle.cs
@@ -9,6 +9,27 @@ public class BossBattle : MonoBehaviour
     public Transform camPosition;
 
     public float camSpeed;
+
+    public int phase2Int,
+        phase3Int;
+
+    public float acitveTime,
+        fadeOutTime,
+        inactiveTime;
+
+    private float activeCounter,
+        fadeCounter,
+        inactiveCounter;
+
+    public Transform[] spawnPoints;
+    private Transform targetPoint;
+    public float moveSpeed;
+
+    public Animator anim;
+
+    public Transform boss;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +37,8 @@ public class BossBattle : MonoBehaviour
 
         //disabling CameraController so it stops following player during boss battle
         cam.enabled = false;
+
+        activeCounter = acitveTime;
     }
 
     // Update is called once per frame
@@ -23,6 +46,41 @@ public class BossBattle : MonoBehaviour
     {
         cam.transform.position =
             Vector3.MoveTowards(cam.transform.position, camPosition.position, camSpeed * Time.deltaTime);
+
+        //phase 1 boss behavior
+        if (BossHealthContoller.instance.currentHealth> phase2Int)
+        {
+            if (activeCounter> 0)
+            {
+                activeCounter -= Time.deltaTime;
+                if (activeCounter <= 0)
+                {
+                    fadeCounter = fadeOutTime;
+                    anim.SetTrigger("vanish");
+                }
+            }
+            else if(fadeCounter > 0)
+            {
+                fadeCounter -= Time.deltaTime;
+                if (fadeCounter <= 0)
+                {
+                    boss.gameObject.SetActive(false);
+
+                    inactiveCounter = inactiveTime;
+                }
+            }
+            else if (inactiveCounter>0)
+            {
+                inactiveCounter -= Time.deltaTime;
+                if (inactiveCounter <= 0)
+                {
+                    boss.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+                    boss.gameObject.SetActive(true);
+
+                    activeCounter = acitveTime;
+                }
+            }
+        }
     }
 
     public void EndBattle()
